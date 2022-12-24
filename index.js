8db17a16a446855e2fd24769934046e09c542a13
Rafael <81846897+permitted-memories@users.noreply.github.com>
2022-12-24 13:30:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+function openTab(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Coisas da lista pessoal
+const texto = document.querySelector('input')
+const btnInsert = document.querySelector('.divInsert button')
+const btnDeleteAll = document.querySelector('.header button')
+const ul = document.querySelector('ul')
+
+var itensDB = []
+
+btnDeleteAll.onclick = () => {
+  itensDB = []
+  updateDB()
+}
+
+texto.addEventListener('keypress', e => {
+  if (e.key == 'Enter' && texto.value != '') {
+    setItemDB()
+  }
+})
+
+btnInsert.onclick = () => {
+  if (texto.value != '') {
+    setItemDB()
+  }
+}
+
+function setItemDB() {
+  if (itensDB.length >= 20) {
+    alert('Limite máximo de 20 itens atingido!')
+    return
+  }
+
+  itensDB.push({ 'item': texto.value, 'status': '' })
+  updateDB()
+}
+
+function updateDB() {
+  localStorage.setItem('todolist', JSON.stringify(itensDB))
+  loadItens()
+}
+
+function loadItens() {
+  ul.innerHTML = "";
+  itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
+  itensDB.forEach((item, i) => {
+    insertItemTela(item.item, item.status, i)
+  })
+}
+
+function insertItemTela(text, status, i) {
+  const li = document.createElement('li')
+  
+  li.innerHTML = `
+    <div class="divLi">
+      <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i});" />
+      <span data-si=${i}>${text}</span>
+      <button onclick="removeItem(${i})" data-i=${i}><i class='bx bx-trash'></i></button>
+    </div>
+    `
+  ul.appendChild(li)
+
+  if (status) {
+    document.querySelector(`[data-si="${i}"]`).classList.add('line-through')
+  } else {
+    document.querySelector(`[data-si="${i}"]`).classList.remove('line-through')
+  }
+
+  texto.value = ''
+}
+
+function done(chk, i) {
+
+  if (chk.checked) {
+    itensDB[i].status = 'checked' 
+  } else {
+    itensDB[i].status = '' 
+  }
+
+  updateDB()
+}
+
+function removeItem(i) {
+  itensDB.splice(i, 1)
+  updateDB()
+}
+
+loadItens()
